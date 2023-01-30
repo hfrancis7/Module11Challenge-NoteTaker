909984f767a72d318f4e3eddbf77e49f2985dbf2
Hailey Francis <n01402670@unf.edu>
2023-01-29 19:32:52 -0500

    added comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,22 @@
-const router = require("express").Router();
-const fs = require("fs");
-const util = require("util");
-const readFile = util.promisify(fs.readFile);
+const router = require("express").Router(); //router
+const fs = require("fs"); //filereader
+const util = require("util"); //util
+const readFile = util.promisify(fs.readFile); //turns readFile into a promise, as recommended by tutor
 
+//function getNotes
+//reads db.json file in utf-8 format, then it turns the notes into rawNotes by parsing the json file
 const getNotes = () => {
     return readFile("db/db.json", "utf-8").then(rawNotes => [].concat(JSON.parse(rawNotes)));
 }
+//getting the notes using getNotes reading the notes from json
 router.get("/notes", (req, res) => {
     getNotes().then(notes => res.json(notes));
 })
+
+//post the notes
+//getNotes console.log notes
+//console.log notes
+//console.log req body
 router.post("/notes", (req, res) => {
     getNotes().then(notes => {
         console.log(notes);
--- routes/html/index.js
+++ routes/html/index.js
@@ -1,12 +1,14 @@
-const router = require("express").Router();
-const path = require("path");
+const router = require("express").Router(); //router
+const path = require("path"); //path
 
+//index html route
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
+//notes html route
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
 
-module.exports = router;
+module.exports = router; //export router
