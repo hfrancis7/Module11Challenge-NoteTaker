f8a33875b9543e1d10652481508b726a89ea57a2
Hailey Francis <n01402670@unf.edu>
2023-01-31 20:17:49 -0500

    added functionality to adding new note to the list, rewriting the json file, which has the appended new note

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,8 @@ const router = require("express").Router(); //router
 const fs = require("fs"); //filereader
 const util = require("util"); //util
 const readFile = util.promisify(fs.readFile); //turns readFile into a promise, as recommended by tutor
+const path = require('path');
+
 
 //function getNotes
 //reads db.json file in utf-8 format, then it turns the notes into rawNotes by parsing the json file
@@ -13,14 +15,29 @@ router.get("/notes", (req, res) => {
     getNotes().then(notes => res.json(notes));
 })
 
-//post the notes
-//getNotes console.log notes
-//console.log notes
-//console.log req body
+//post the new notes into the html list
 router.post("/notes", (req, res) => {
     getNotes().then(notes => {
-        console.log(notes);
-        console.log(req.body);
+        let notesArr = notes; //notes array
+        const { title, text } = req.body; //get the title and stuff from notes
+
+        if(title && text){
+            const newNote = { //create a new note (TODO: needs identifier...)
+                title,
+                text
+            }
+            notesArr.push(newNote); //pushes the new note onto the list
+        }else{ //there already appears to be a safeguard in the html (or at least within the code already given), putting this here just in case.
+            alert("Please make sure both the title field and the note field are filled out.");
+        }
+
+        let notesStr = JSON.stringify(notesArr); //turns the json notes array into a string
+
+        //writes the notes information into the json file (Not formatted though... wonder if that matters?)
+        fs.writeFile("db/db.json", notesStr, (err) =>
+        err ? console.error(err) : console.log('\nSuccess.\n'));
+        
+        res.redirect('back'); //refreshes the page to display the new note created
     });
 })
 module.exports = router;
