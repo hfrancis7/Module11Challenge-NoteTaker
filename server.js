9a6d28d1e7686681193f81beff25becf67e6efd8
Hailey Francis <n01402670@unf.edu>
2023-02-02 20:06:22 -0500

    changed port for local heroku:

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express"); //require express
 const routes = require("./routes"); //require everything in routes directory
 const path = require("path"); //require path
 const app = express(); //ap is express
-const PORT = 3001; //port num
+const PORT = 5000; //port num
 
 app.use(express.json()); //use express.json middleware
 app.use(express.urlencoded({extended: false})); //use urlecoded middleware
